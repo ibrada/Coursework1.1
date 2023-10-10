df111d6cae0b990573177dba8cdff29fe9225a5b
ibrada <radikibragimov@mail.ru>
2023-10-11 01:47:15 +0300

    cs1.1 is done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
 public class Main {
+
+    public static boolean palindrom(String word) {
+        String wordDefault = word.toLowerCase().replace(" ", "").replace(",", "");
+        StringBuilder wordPrepared = new StringBuilder(wordDefault);
+        StringBuilder wordReverse = wordPrepared.reverse();
+
+        return (wordReverse.toString().equals(wordDefault));
+    }
+
     public static void main(String[] args) {
+
+        String word = "Wow, Level Wow";
+        String word2 = "Ser, yes, ser";
+
+        System.out.println(palindrom(word));
+        System.out.println(palindrom(word2));
+
     }
 }
